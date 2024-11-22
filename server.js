0d891b1b82f9cfe0d2adb43a94f18049f35e768e
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-11-22 13:01:54 -0300

    created findById

--- server.js
+++ server.js
@@ -2,33 +2,40 @@ import express from "express";
 
 const posts = [
   {
-    descricao: "Foto texte",
+    id: 1,
+    descricao: "Gato deitado na cama",
     imagem: "https://placecats.com/300/150",
   },
   {
+    id: 2,
     descricao: "Gato fofo dormindo",
     imagem: "https://placecats.com/300/150",
   },
   {
-    descricao: "Paisagem montanhosa",
-    imagem: "https://placecats.com/300/150",
-  },
-  {
-    descricao: "Cachorro brincando",
-    imagem: "https://placecats.com/300/150",
-  },
-  {
-    descricao: "Comida deliciosa",
+    id: 3,
+    descricao: "Gato mal humorado",
     imagem: "https://placecats.com/300/150",
   },
 ];
 
 const app = express();
+app.use(express.json()); // convertendo todos os textos em json
 
 app.listen(3000, () => {
   console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-  res.status(200).send("Imersão Alura...");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+function findById(id) {
+  return posts.findIndex((posts) => {
+    return posts.id == Number(id);
+  });
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = findById(req.params.id);
+  res.status(200).json(posts[index]);
 });
