de6e2e37e1111f234718cf8c6c51251e12b160e0
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-11-22 12:19:16 -0300

    seed test

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 import express from "express";
 
+const posts = [
+  {
+    descricao: "Foto texte",
+    imagem: "https://placecats.com/300/150",
+  },
+  {
+    descricao: "Gato fofo dormindo",
+    imagem: "https://placecats.com/300/150",
+  },
+  {
+    descricao: "Paisagem montanhosa",
+    imagem: "https://placecats.com/300/150",
+  },
+  {
+    descricao: "Cachorro brincando",
+    imagem: "https://placecats.com/300/150",
+  },
+  {
+    descricao: "Comida deliciosa",
+    imagem: "https://placecats.com/300/150",
+  },
+];
+
 const app = express();
 
 app.listen(3000, () => {
